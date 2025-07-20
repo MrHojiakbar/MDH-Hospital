f6742a642929c4c915988fd9b3970a98b14e6361
Dilmuhammad <abdumalikovdilmuhammad06@gmail.com>
2025-07-20 16:19:31 +0500

    navbar

--- client/src/layout/navbar/index.jsx
+++ client/src/layout/navbar/index.jsx
@@ -1,12 +1,65 @@
-import React from 'react'
-import "./index.scss"
+import React from "react";
+import "./index.scss";
+import { NavLink } from "react-router";
 
-const NavBarLaout = () => {
+const NavBarLayout = () => {
   return (
-   <nav>
-        <p>navbar</p>
-   </nav>
-  )
-}
+    <nav>
+      <div className="img"></div>
+      <div className="navigation">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Asosiy
+        </NavLink>
+        <NavLink
+          to="/doctors"
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Doktorlar
+        </NavLink>
+        <NavLink
+          to="/statistics"
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Statistika
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Biz haqimizda
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Aloqa uchun
+        </NavLink>
+      </div>
+      <div className="register">
+        <NavLink
+          to="/profile"
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Profil
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
 
-export default NavBarLaout
+export default NavBarLayout;
