b32fa628f0f3b123e7aa50433801540f1a841091
Dilmuhammad <abdumalikovdilmuhammad06@gmail.com>
2025-07-22 13:55:14 +0500

    register

--- server/src/modules/users/users.controller.ts
+++ server/src/modules/users/users.controller.ts
@@ -11,12 +11,16 @@ import { UsersService } from './users.service';
 import { LoginDto, RegisterDto } from './dtos';
 import { Request, Response } from 'express';
 import { AuthGuard } from '@nestjs/passport';
+import { Protected, Roles } from 'src/decaratores';
+import { userRole } from '@prisma/client';
 
 @Controller('auth')
 export class UsersController {
   constructor(private readonly service: UsersService) {}
 
   @Post('register')
+  @Protected(false)
+  @Roles([userRole.admin, userRole.doctor, userRole.manager, userRole.user])
   async register(
     @Body() payload: RegisterDto,
     @Res({ passthrough: true }) res: Response,
@@ -25,6 +29,8 @@ export class UsersController {
   }
 
   @Post('login')
+  @Protected(false)
+  @Roles([userRole.admin, userRole.doctor, userRole.manager, userRole.user])
   async login(
     @Body() payload: LoginDto,
     @Res({ passthrough: true }) res: Response,
@@ -33,11 +39,15 @@ export class UsersController {
   }
 
   @Get('google')
+  @Protected(false)
+  @Roles([userRole.admin, userRole.doctor, userRole.manager, userRole.user])
   @UseGuards(AuthGuard('google'))
   async gooogle() {}
 
   @Get('google/callback')
   @UseGuards(AuthGuard('google'))
+  @Protected(false)
+  @Roles([userRole.admin, userRole.doctor, userRole.manager, userRole.user])
   async googleCallback(
     @Res({ passthrough: true }) res: Response,
     @Req() req: Request & { user: any },
@@ -46,11 +56,15 @@ export class UsersController {
   }
 
   @Get('me')
+  @Protected(true)
+  @Roles([userRole.admin, userRole.doctor, userRole.manager, userRole.user])
   async me(@Req() req: { userId: string }) {
     return await this.service.me(req.userId);
   }
 
   @Get('logout')
+  @Protected(true)
+  @Roles([userRole.admin, userRole.doctor, userRole.manager, userRole.user])
   async logout(@Res() res: Response) {
     res.clearCookie('accessToken');
     res.clearCookie('refreshToken');
