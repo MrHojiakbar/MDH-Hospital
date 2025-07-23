5f9f28b6d00a2f076218a1e2f5cd24164fa54f46
Muhammad Yusuf <yuvsufn@gmail.com>
2025-07-23 13:52:13 +0500

    update

--- client/src/pages/DoctorDetails/index.jsx
+++ client/src/pages/DoctorDetails/index.jsx
@@ -1,19 +1,46 @@
-
-import React from 'react'
-import { DoctorDetailsWrapper } from './DoctorDetails.styled';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from "react";
+import { DoctorDetailsWrapper } from "./DoctorDetails.styled";
+import { useParams } from "react-router";
+import { customAxios } from "../../api";
+import toast from "react-hot-toast";
 
 const DoctorDetailsPage = () => {
-
+  const [doctor, setDoctorInfo] = useState({});
   const { doctorId } = useParams();
 
-  console.log(doctorId)
+  useEffect(() => {
+    async function getData() {
+      try {
+        const data = await customAxios.get(`/doctors/${doctorId}`);
+        console.log(data.data.data);
+        setDoctorInfo(data.data.data);
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
+    }
+
+    getData();
+  }, [doctorId]);
 
   return (
-    <DoctorDetailsWrapper>
-      DoctorDetailsPage
-    </DoctorDetailsWrapper>
-  )
-}
+    <DoctorDetailsWrapper className="container">
+      <div className="main-box">
+        <div className="box">
+          <div className="box-img">
+            <img
+              src="https://thumbs.dreamstime.com/b/african-male-doctor-happy-tablet-computer-34481166.jpg"
+              alt=" this Doctor img"
+            />
+          </div>
 
-export default DoctorDetailsPage
+          <div className="box-words">
+            <p>Ism Familyasi: {doctor?.user?.fullname}</p>
+            <p>Ish Tajribasi: {doctor?.experienceYears} Yil</p>
+          </div>
+        </div>
+      </div>
+    </DoctorDetailsWrapper>
+  );
+};
+
+export default DoctorDetailsPage;
--- server/src/decaratores/protected.decorator.ts
+++ server/src/decaratores/protected.decorator.ts
@@ -3,4 +3,4 @@ import { SetMetadata } from "@nestjs/common";
 
 export const PROTECTED_KEY = 'PROTECTED';
 
-export const Protected = (isProtected: Boolean) => SetMetadata(PROTECTED_KEY, isProtected);
+export const Protected = (isProtected: boolean) => SetMetadata(PROTECTED_KEY, isProtected);
